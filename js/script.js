cedb5b3ee6f07e9bbf43bbf57f57bb9e86c2044d
Alex979 <alexbroaddus1@gmail.com>
2018-09-05 13:38:23 -0400

    more cool animations

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-TweenLite.from('#vr', 1, {
-    opacity: 0,
-    x: 20,
-    delay: 0.5,
-});
-TweenLite.from('#ar', 1, {
+TweenLite.from('#vr', 2, {
     opacity: 0,
     x: 20,
     delay: 1,
+    ease: Power4.easeOut,
 });
-TweenLite.from('#mr', 1, {
+TweenLite.from('#ar', 2, {
     opacity: 0,
     x: 20,
-    delay: 1.5,
+    delay: 2,
+    ease: Power4.easeOut,
+});
+TweenLite.from('#mr', 2, {
+    opacity: 0,
+    x: 20,
+    delay: 3,
+    ease: Power4.easeOut,
+});
+TweenLite.from('#headset1', 2, {
+    opacity: 0,
+    scale: 0.9,
+    ease: Power4.easeOut,
 });
