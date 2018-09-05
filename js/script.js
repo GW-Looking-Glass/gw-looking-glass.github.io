856a880edbd747159e28c116da8e72e17a76c167
Alex979 <alexbroaddus1@gmail.com>
2018-09-05 13:30:40 -0400

    added cool animations

---
+++ js/script.js
@@ -0,0 +1,15 @@
+TweenLite.from('#vr', 1, {
+    opacity: 0,
+    x: 20,
+    delay: 0.5,
+});
+TweenLite.from('#ar', 1, {
+    opacity: 0,
+    x: 20,
+    delay: 1,
+});
+TweenLite.from('#mr', 1, {
+    opacity: 0,
+    x: 20,
+    delay: 1.5,
+});
